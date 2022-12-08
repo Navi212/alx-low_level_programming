c2ada933f81563ebb8aedf83b8f1aac291b869b7
Joseph Chris <josephchris615@gmail.com>
2022-12-08 15:59:50 -0800

    Amended print string with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * main - Prints a string with Puts
  *
- * Return - 0 (Success);
+ * Return -  (Success);
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	
-	return (0);
+        puts("Programming is like building a multilingual puzzle \n");
+        return (0);
 }
