809524d320f7d6e65de9ff80a60adf014643c609
Joseph Chris <josephchris615@gmail.com>
2022-12-08 15:55:17 -0800

    Print a string with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string with Puts
+ *
+ * Return - 0 (Success);
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return (0);
+}
