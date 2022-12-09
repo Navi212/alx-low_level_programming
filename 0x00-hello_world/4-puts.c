fe16c588825fd146859db16cfeadec362ee7ea47
Joseph Chris <josephchris615@gmail.com>
2022-12-08 16:52:16 -0800

    Amendement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - A function that prints strings.
  * Description : Print strings with puts().
- * Return : Return zero on success.
+ * Return : Return value is 0 meaning success.
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("Programming is like building a multilingual puzzle \n");
 	return (0);
 }
