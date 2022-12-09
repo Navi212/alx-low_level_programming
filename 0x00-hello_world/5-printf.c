2cedc496edf290c501a4bd28d207d78e703536d4
Joseph Chris <josephchris615@gmail.com>
2022-12-08 17:47:02 -0800

    Print string with printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints phrase with proper grammer, but the outcome is a piece of art,
- * Return : Returns 0 if exited properly but non zero otherwise
+ * Return : 0 if exited properly but non zero otherwise
  */
 int main(void)
 {
