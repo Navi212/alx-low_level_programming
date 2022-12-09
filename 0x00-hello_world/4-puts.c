84c5c721ff25200fbee8dcb1c104c4ae0dda030b
Joseph Chris <josephchris615@gmail.com>
2022-12-08 16:56:49 -0800

    Amended print string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - A function that prints strings.
  * Description : Print strings with puts().
- * Return : Return value is 0 meaning success.
- *
+ * Return : Return 0 (Success)
  */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle \n");
