39c4cebdc372e5b08dfaf90edbce49060b618828
Joseph Chris <josephchris615@gmail.com>
2022-12-08 18:01:50 -0800

    Print string with printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the exact phrase with proper grammer,
- * but the outcome is a piece of art,
- * Return : 0 if exited successfully but non zero value
+ * main - Prints exact phrase with proper grammer, but
+ * the outcome is a piece of art,
+ * Description : This is a program that prints the phrase
+ * Return: 0 if exited properly, non zero if otherwise
  */
 int main(void)
 {
