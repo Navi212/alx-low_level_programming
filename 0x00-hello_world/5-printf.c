37ef95dac18caeee5dd514452685ba7597bd0c0b
Joseph Chris <josephchris615@gmail.com>
2022-12-08 17:56:29 -0800

    Print string with printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,9 @@
  * main - Prints the exact phrase with proper grammer,
  * but the outcome is a piece of art,
  * Return : 0 if exited successfully but non zero value
- * if otherwise
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
