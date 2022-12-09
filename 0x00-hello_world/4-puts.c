52a9126ba8df27e33b70f6037b55a2c12bcbea7c
Joseph Chris <josephchris615@gmail.com>
2022-12-08 16:11:39 -0800

    Amended print string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  * Description: This a function that prints a string
  * with the puts() function.
  *
- * Return - 0 Success
+ * Return - The return value zero signifies sucess.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
