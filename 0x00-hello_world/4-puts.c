b9350fae46707ca28539b7d6585d02d2869698c0
Joseph Chris <josephchris615@gmail.com>
2022-12-08 17:21:23 -0800

    Amended print string with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
- * main - Entry point
- *
- * Description : Print strings with puts().
- * Return : Return 0 (Success)
+ * main - Entry point of our program.
+ * Description: A program to print string with puts
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle \n");
 	return (0);
 }
