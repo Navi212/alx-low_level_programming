a6a7b3ca09267e6cd9791ad9510c8e9895db9b45
Joseph Chris <josephchris615@gmail.com>
2022-12-13 11:40:48 -0800

    Prints alpha in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of our program.
+ *
+ * Description: A function that prints the alphabet in lowercase.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha >= 'a' && alpha <= 'z')
+	{
+		_putchar(alpha);
+		++alpha;
+	}
+
+	_putchar('\n);
+}
