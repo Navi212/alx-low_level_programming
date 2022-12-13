3ce717eadb3cc2ecb1f547188b9b8e293ed17e22
Joseph Chris <josephchris615@gmail.com>
2022-12-13 12:15:36 -0800

    Print alpha 10times in lwrcase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A function that prints alphabet.
+ *
+ * Description: Prints alphabet 10 times in lowercase.
+ * Return: 0 on success.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 0)
+	{
+		char alpha = 'a';
+
+		while (alpha >= 'a' && alpha <= 'a')
+		{
+			_putchar(alpha);
+			++alpha;
+		}
+
+		_putchar('\n');
+		++i;
+	}
+}
