9f753e81a9882ac1d40d2d16cee602a1d3064e07
Joseph Chris <josephchris615@gmail.com>
2022-12-13 20:37:19 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - A function that prints 9 multipliaction
+ * table.
+ * Return: 0 on Success.
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; ++a)
+	{
+		for (b = 0; b <= 9; ++b)
+		{
+			c = a * b;
+
+			if ((c / 10) == 0)
+
+				if (b != 0)
+					_putchar(' ');
+				_putchar(c + '0');
+
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
