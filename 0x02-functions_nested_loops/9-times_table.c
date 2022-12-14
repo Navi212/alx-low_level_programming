76a385a0c4a6da9e6bb5c294bc89df593db5fc4f
Joseph Chris <josephchris615@gmail.com>
2022-12-14 13:44:17 -0800

    Print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,51 @@
 #include "main.h"
 
 /**
- * times_table - A function that prints 9 multipliaction
+ * times_table - A function that prints 9 multiplcation table.
  * table.
  * Return: 0 on Success.
  */
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+	int limit;
 
+	for (j = 0; j <= limit; j++)
+	{
+		for (i = 0; i <= limit; i++)
+		{
+			r = i * j;
+
+			if (r < 10 && i < limit)
+			{
+				_putchar((r % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+
+			else if (r < 10 && i >= limit)
+			{
+				_putchar((r % 10) + '0');
+			}
+
+			else if (r >= 10 && i < limit)
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
