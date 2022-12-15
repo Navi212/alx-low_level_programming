5934e7c703d0d6864f57531224bf999b50df9236
Joseph Chris <josephchris615@gmail.com>
2022-12-15 15:03:37 -0800

    Adds fourth test

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,11 @@
  * Descrition: This function prints numbers from 0 through 9
  * Return: 0 on success
  */
-
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-	{
+	for (i = 0; i <= 9; ++i)
 		_putchar('0' + i);
-	}
 	_putchar('\n');
 }
