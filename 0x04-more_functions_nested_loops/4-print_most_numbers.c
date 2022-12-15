1633eb3c931f5b254031fda8365b19081eaeb419
Joseph Chris <josephchris615@gmail.com>
2022-12-15 13:58:48 -0800

    Adds fourth test

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function that prints 0-9 followed
+ * by a new line excluding 2 and 4
+ * Return: 0 on success
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
