5c83bd20d6dc30b7469851076e40aed92df8b697
Joseph Chris <josephchris615@gmail.com>
2022-12-15 13:33:32 -0800

    Adds print ints 0-9 test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints 0-9 followed
+ * by a new line
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
