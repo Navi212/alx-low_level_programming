8139878021d4de20ac79ea580067a2f224fe872b
Joseph Chris <josephchris615@gmail.com>
2022-12-15 13:28:34 -0800

    Adds multiply test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: Argument 1
+ * @b: Argument 2
+ * Return: 0 on success
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
