e7ba4533fa946374abd3a58e4c793af7a575d822
Joseph Chris <josephchris615@gmail.com>
2022-12-15 18:29:58 -0800

    Adds FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,26 @@ void fizzbuzz(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 
 		else
 		{
 			printf("%d", i);
-		}
-
-		if (i != 100)
-		{
 			printf(" ");
 		}
+
 	}
 }
