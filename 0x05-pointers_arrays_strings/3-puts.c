3ceaf48d4be0a7c50e1a7b3e30ed1417c3e06cbc
Joseph Chris <josephchris@gmail.com>
2022-12-19 23:54:46 +0000

    Fourth test print str to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+/**
+ * _puts - Prints string to stdout.
+ * @str: Pointer argument.
+ *
+ * Return: Nothing.
+ */
+
+void _puts(char *str)
+{
+	char cha;
+
+	while (*str != '\0')
+	{
+		cha = *str;
+		_putchar(cha);
+
+		str++;
+	}
+
+	_putchar('\n');
+}
