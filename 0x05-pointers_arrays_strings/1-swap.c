2e3b7941f3cd6547509268af17afe81520b043b5
Joseph Chris <josephchris@gmail.com>
2022-12-19 22:37:24 +0000

    Second test swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - Swaps the values of two integers.
+ * @*a: Pointer argument a.
+ * @*b: Pointer argument b.
+ *
+ * Return: Nothing.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
