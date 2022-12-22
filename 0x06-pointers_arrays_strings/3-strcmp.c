235725e8c25cfadd0dadf7a88037a9fef2fb19d3
Joseph Chris <josephchris@gmail.com>
2022-12-22 23:33:20 +0000

    Fourth test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,9 +14,9 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s2 == '\0')
 			return (*s1);
-		else if (*s2 > *s1)
+		if (*s2 > *s1)
 			return (*s1 - *s2);
-		else
+		if (*s1 > *s2)
 			return (*s1 - *s2);
 
 		s1++;
