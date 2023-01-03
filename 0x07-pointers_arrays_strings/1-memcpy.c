ec2f56ff2dc5a03c77b8ef3969bbb74b5494951a
Joseph Chris <josephchris@gmail.com>
2023-01-03 19:41:07 +0000

    Adds memcpy file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,8 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (src = src, dest = dest; (unsigned int)*src < n; src++, dest++)
-		*dest = *src;
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
