94fa0927dbd764bab98c3a7f45197ccdc0a3d0c9
Joseph Chris <josephchris@gmail.com>
2023-01-03 22:17:01 +0000

    Adds set_string file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value to a pointer to
+ * a char
+ * @s: input
+ * @to: input
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
