2dd3569d6106ee7395221a3840754d0c12b74d57
Joseph Chris <josephchris@gmail.com>
2023-01-04 00:52:10 +0000

    Adds strspn file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strspn  gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: input string to check
  * @accept: input string
  *
@@ -10,8 +10,8 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int c = 0;
+	unsigned int i, j;
+	unsigned int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -20,12 +20,12 @@ unsigned int _strspn(char *s, char *accept)
 			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
-					c++;
+					count++;
 			}
 		}
 		else
-			return (c);
+			return (count);
 
 	}
-	return (c);
+	return (count);
 }
