a01969fe80deada51b4584298c9a6c30363d2fbe
Joseph Chris <josephchris@gmail.com>
2023-01-04 23:03:43 +0000

    Adds 1-print_rev_recursion.c file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,10 +10,9 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_print_rev_recursion(++s);
-		_putchar(*s);
-	}
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
