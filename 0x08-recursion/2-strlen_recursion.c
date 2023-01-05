339935b09dd0220ecfffbc93df0816111b59c444
Joseph Chris <josephchris@gmail.com>
2023-01-05 00:03:32 +0000

    Adds 2-strlen_recursion.c file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,15 +9,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s == '\0')
 		return (0);
-
-	else
-	{
-		i += 1;
-		_strlen_recursion(++s);
-		return (i);
-	}
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
