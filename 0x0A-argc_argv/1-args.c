172c020459cb1ff502b352fb6c50418a34d12ba9
Joseph Chris <josephchris@gmail.com>
2023-01-07 00:51:05 +0000

    Adds file that counts args passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints number of arguments passed to it
+ * @args: counts arguments
+ * @argv: contains arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc >= 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
