cd8682e784d009c665604f2a624ac8b878969f40
Joseph Chris <josephchris@gmail.com>
2023-01-08 01:36:52 +0000

    Removed strlen from 7-puts file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - A function that returns the length of a string.
- * @s: Pointer argument.
- *
- * Return: Length of string.
- */
-
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-	{
-		count++;
-		s++;
-	}
-
-	return (count);
-}
-
-/**
  * puts_half - prints half of a string
  * @str: string
  *
