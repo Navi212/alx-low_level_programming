9df80fd496064cb00f6acb40343321bfacb07543
Joseph Chris <josephchris@gmail.com>
2023-01-08 00:57:06 +0000

    Adds put_half file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ *
+ * Return: nothing
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len, n;
+
+	len = _strlen(str);
+	if (len % 2 != 0)
+	{
+		for (n = (len - 1) / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (n = len / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+		_putchar('\n');
+	}
+}
