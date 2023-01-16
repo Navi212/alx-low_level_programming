1ab2867dda3b025edf8d984796eb13b331c32e2a
Joseph Chris <josephchris@gmail.com>
2023-01-16 21:35:06 +0000

    Updates 3-function_like_macro file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-/*
- * File: 3-function_like_macro.h
- * Auth: Joseph Chris N
- * Desc: Header file that defines a function-like macro ABS(x)
- *       and computes absolute value of a number x.
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 #endif
