ef2f50767a1d319a6d65ca0edde31bab6555e44f
Joseph Chris <josephchris@gmail.com>
2023-01-19 11:46:31 +0000

    Updates 2-int_index file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size > 0 && array != NULL && cmp != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
 		{
 			return (i);
 		}
+	}
 	}
 
 	return (-1);
