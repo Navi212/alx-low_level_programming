e05ecb3eb78cc7075342842925e846e86e43c4bc
Joseph Nweke <josephchristo615@gmail.com>
2023-01-31 16:20:58 +0000

    Updates 0-print_listint.c file with %u

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int count;
+	size_t count;
 
 	if (h == NULL)
 		return (0);
 	for (count = 0; h != NULL; count++)
 	{
-		printf("%d\n", h->n);
+		printf("%u\n", h->n);
 		h = h->next;
 	}
 	return (count);
