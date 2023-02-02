6b9ebcdb630c981c8c934e0d6cdfbc6da2db0e54
Joseph Nweke <josephchristo615@gmail.com>
2023-02-02 17:46:28 +0000

    Adds task 1 file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * binary_to_uint - Converters a binary number to an unsigned integer
@@ -20,19 +20,19 @@ unsigned int binary_to_uint(const char *b)
 	pow = 1;
 	if (b == NULL)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
+	for (len = 0; b[len] != '\0'; len++)
 		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
 		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
+		for (j = len - 1; j > 0; j--)
 			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
+		sum = sum + (pow * (b[i] - 48));
 		len--;
-		pow = 1; /*Reseting the exponent*/
+		pow = 1;
 	}
 	return (sum);
 }
