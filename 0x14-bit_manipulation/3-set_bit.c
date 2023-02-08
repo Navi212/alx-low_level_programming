6813e6598f55f27383512980e9643d178435937c
Joseph Nweke <josephchristo615@gmail.com>
2023-02-08 15:14:58 +0000

    Updates task 2-4 files with main.h

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -17,7 +17,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n = (1 << index) | num;
+	*n = (1 << index) | *n;
 
 	return (1);
 }
