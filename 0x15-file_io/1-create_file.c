21fef1c9a789c422960ffb68ab2beecdc7ee5b90
Joseph Nweke <josephchristo615@gmail.com>
2023-02-10 11:32:46 +0000

    Updates 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,15 +15,15 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-		if (fd == -1)
-			return (-1);
+		for (i = 0; text_content[i]; i++)
+			;
 	}
 
-	for (i = 0; text_content[i]; i++)
-		;
+	fd = open(filename, O_CREAT | O_RDWR | TRUNC);
+	if (fd == -1)
+		return (-1);
 
 	num = write(fd, text_content, i);
 	if (num == -1)
