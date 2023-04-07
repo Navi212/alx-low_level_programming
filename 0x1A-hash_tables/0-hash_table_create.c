7b5b3752019f8480229529dd79bdeb51aea26845
Nweke Joseph <josephchristo615@gmail.com>
2023-04-07 20:51:12 +0100

    Removed commented block in task 0 file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
-	/*unsigned long int i = 0;*/
+	unsigned long int i = 0;
 
 	hash_t = malloc(sizeof(hash_table_t));
 	if (!hash_t)
