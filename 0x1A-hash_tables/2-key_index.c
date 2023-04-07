48da6d66fd81b336f988c5dba84d1532a997814e
Nweke Joseph <josephchristo615@gmail.com>
2023-04-07 21:15:47 +0100

    Updates task2 file removing semicolon

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  *
  * Return: key index location
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
