cad3c37367efeffd551fdc39405613fd11265797
Nweke Joseph <josephchristo615@gmail.com>
2023-04-07 20:59:56 +0100

    Adds task 1 file

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - A hash function that implements djb2
+ * hashing algorithm
+ * @str: input string to hash
+ *
+ * Return: Hashed value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hashd_val;
+	/*int i;*/
+
+	hashd_val = 5381;
+	while ((i = *str++))
+		hashd_val = ((hashd_val << 5)) + i;
+	
+	/*return (hashd_val);*/
+}
