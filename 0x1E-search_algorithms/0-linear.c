4059c7902c8e0981469e0dadedad2cbbbd11478e
Navi212 <josephchristo615@gmail.com>
2023-09-13 14:53:59 +0100

    Removed excess lines

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		printf("value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
